fa87cac3e28bebb936e0a6482c7597393c41135a
Slavica-lab <slavica-alvir@hotmail.com>
2022-03-02 19:53:23 +0100

    02.03

--- NewsAppG3.5/src/main/java/at/ac/fhcampuswien/Article.java
+++ NewsAppG3.5/src/main/java/at/ac/fhcampuswien/Article.java
@@ -1,4 +1,19 @@
 package at.ac.fhcampuswien;
 
 public class Article {
+    String author;
+    String title;
+
+    public Article(String author, String title){
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getTitle(){
+        return title;
+    }
 }
